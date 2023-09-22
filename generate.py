afad20ca6fec472e74be5b1a920b360e6404651d
vinothkumar arumugam <132250443+vinothkumarArumugam@users.noreply.github.com>
2023-09-22 09:00:30 -0400

    generate.py

---
+++ generate.py
@@ -0,0 +1,16 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','crudproject.settings')
+import django
+django.setup()
+from crudapp.models import *
+from faker import Faker
+from random import *                                              ##### to generate fake data for huge amount of data creation,testing purpose
+faker =Faker()
+def generate(n):
+    for i in range(n):
+        fname=faker.name()
+        fage=randint(1,100)
+        fcity=faker.city()
+        fmarks=randint(30,100)
+        student_record=Student.objects.get_or_create(name=fname,age=fage,city=fcity,marks=fmarks)
+generate(15)
